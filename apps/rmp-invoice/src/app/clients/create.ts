744230923a9e8846498571b2a879d7fafc2af495
nikhilkuyya <nikhilkuyya@gmail.com>
2026-06-23 14:20:07 +0530

    feat: create the client

--- apps/rmp-invoice/src/app/clients/create.ts
+++ apps/rmp-invoice/src/app/clients/create.ts
@@ -1,28 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { Component, inject, signal } from '@angular/core';
+import { form, FormField, submit } from '@angular/forms/signals';
 import { RMPCreateClientPaylod } from '@rmp/shared-models';
-import { RMPInput } from '@rmp/shared-ui-ang';
+import { RMPInput, RmpButtonDirective } from '@rmp/shared-ui-ang';
 import { NewRMPAddress } from './address';
+import { ClientService } from '../services/client.service';
+import { firstValueFrom } from 'rxjs';
 @Component({
   selector: 'rmp-create-client',
   templateUrl: './create.html',
-  imports: [RMPInput, FormField, CommonModule, NewRMPAddress],
+  imports: [RMPInput, FormField, CommonModule, NewRMPAddress, RmpButtonDirective],
 })
 export class CreateRMPClient {
-    
+  private clientService = inject(ClientService);
+
   rmpClientModel = signal<RMPCreateClientPaylod>({
     companyName: '',
     companyGSTIn: '',
     address: {
-        addressLine: '',
-        city: '',
-        postalCode: '',
-        state: 'Telangana',
-        country: 'BHARAT',
+      addressLine: '',
+      city: '',
+      postalCode: '',
+      state: 'Telangana',
+      country: 'BHARAT',
     },
     invoiceEmail: '',
   });
 
   rmpClientForm = form(this.rmpClientModel);
+
+  handleSubmit() {
+    submit(this.rmpClientForm, async (form) => {
+      try {
+        await firstValueFrom(this.clientService.createClient(form().value()));
+        form().reset();
+        return undefined;
+      } catch (error) {
+        console.error(error);
+        return undefined;
+      }
+    });
+  }
 }
--- apps/rmp-invoice/src/app/services/client.service.ts
+++ apps/rmp-invoice/src/app/services/client.service.ts
@@ -1,31 +1,37 @@
-import {  inject, Injectable, Resource, Signal } from '@angular/core';
-import { RMPClient } from '@rmp/shared-models';
+import { inject, Injectable, Resource, Signal } from '@angular/core';
+import { RMPClient, RMPCreateClientPaylod } from '@rmp/shared-models';
 import { API_ROOT } from '@rmp/invoice-web-core';
-import { httpResource } from '@angular/common/http';
+import { HttpClient, httpResource } from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClientService {
-    private apiRoot = inject(API_ROOT);
+  private apiRoot = inject(API_ROOT);
+  private httpClient = inject(HttpClient);
+  getClients(params: Signal<{ name: string } | undefined>): Resource<RMPClient[]> {
+    return httpResource(
+      () => {
+        const requestParams = {
+          ...params(),
+        };
+        return {
+          url: `${this.apiRoot}/api/client`,
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        };
+      },
+      {
+        defaultValue: [],
+      },
+    );
+  }
 
-    getClients(params: Signal<{name: string} | undefined>): Resource<RMPClient[]> {
-        return httpResource(
-            () => {
-                const requestParams = {
-                    ...params()
-                }                                
-                return {
-                    url: `${this.apiRoot}/api/client`,
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },                    
-                }                
-            },
-            {
-                defaultValue: []
-            }
-        )
-    }
+  createClient(clientPayload: RMPCreateClientPaylod) {
+    return this.httpClient.post(`${this.apiRoot}/api/client`, {
+      ...clientPayload,
+    });
+  }
 }
